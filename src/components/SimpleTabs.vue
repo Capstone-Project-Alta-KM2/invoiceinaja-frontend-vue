ac5669b6a8c96d0cad321279f75b80eef4b929e8
andri-irawan79 <iandri647@gmail.com>
2022-06-08 16:54:30 +0800

    Fixed tab display of invoice page

---
+++ src/components/SimpleTabs.vue
@@ -0,0 +1,165 @@
+<template>
+    <div class="bg-white">
+        <div class="flex justify-between align-center px-3">
+            <ul
+                class="
+                flex flex-wrap
+                -mb-px
+                text-sm
+                font-medium
+                text-center
+                text-gray-500
+                dark:text-gray-400
+                "
+                id="tabExample"
+                role="tablist"
+            >
+                <li class="mr-2" role="presentation">
+                    <button
+                        :class="currentShow == 'all-invoice' ? ActiveClass : inActiveClass"
+                        id="all-invoice"
+                        type="button"
+                        role="tab"
+                        aria-controls="all-invoice-example"
+                        aria-selected="false"
+                        @click="changeActive('all-invoice')"
+                    >
+                        All Invoice
+                    </button>
+                </li>
+                <li class="mr-2" role="presentation">
+                    <button
+                        :class="currentShow == 'paid' ? ActiveClass : inActiveClass"
+                        id="paid"
+                        type="button"
+                        role="tab"
+                        aria-controls="paid-example"
+                        aria-selected="false"
+                        @click="changeActive('paid')"
+                    >
+                        Paid
+                    </button>
+                </li>
+                <li class="mr-2" role="presentation">
+                    <button
+                        :class="currentShow == 'overdue' ? ActiveClass : inActiveClass"
+                        id="overdue"
+                        type="button"
+                        @click="changeActive('overdue')"
+                    >
+                        Overdue
+                </button>
+                </li>
+                <li role="presentation">
+                    <button
+                        :class="currentShow == 'pending' ? ActiveClass : inActiveClass"
+                        id="pending"
+                        type="button"
+                        @click="changeActive('pending')"
+                    >
+                        Pending
+                    </button>
+                </li>
+                <li role="presentation">
+                    <button
+                        :class="currentShow == 'draft' ? ActiveClass : inActiveClass"
+                        id="draft"
+                        type="button"
+                        @click="changeActive('draft')"
+                    >
+                        Draft
+                    </button>
+                </li>
+            </ul>
+            <div class="flex mr-5">
+                <div class="my-auto">
+                    <button class="mx-2 bg-white text-black px-3 py-1 rounded-lg border-1 border-black">+ Add New</button>
+                </div>
+                <div class="my-auto">
+                    <button class="mx-2 bg-white text-black px-3 py-1 rounded-lg border-1 border-black">Import File</button>
+                </div>
+            </div>
+        </div>
+        <div id="tabContentExample">
+            <div
+                class="px-4 rounded-md"
+                :hidden="
+                currentShow == 'all-invoice' ? (isHidden = false) : (isHidden = true)
+                "
+            >
+                <invoices-table/>
+            </div>
+            <div
+                class="p-4 rounded-md"
+                :hidden="
+                currentShow == 'paid' ? (isHidden = false) : (isHidden = true)
+                "
+            >
+                Table Paid
+            </div>
+            <div
+                class="p-4 rounded-md"
+                :hidden="
+                currentShow == 'overdue' ? (isHidden = false) : (isHidden = true)
+                "
+            >
+                Table Overdue
+            </div>
+            <div
+                class="p-4 rounded-md"
+                :hidden="
+                currentShow == 'pending' ? (isHidden = false) : (isHidden = true)
+                "
+            >
+                Table Pending
+            </div>
+            <div
+                class="p-4 rounded-md"
+                :hidden="
+                currentShow == 'draft' ? (isHidden = false) : (isHidden = true)
+                "
+            >
+                Table Draft
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import InvoicesTable from './InvoicesTable.vue';
+export default {
+    name: 'SimpleTab',
+    components: {
+        InvoicesTable
+        
+    },
+    data() {
+        return {
+        isHidden: false,
+        currentShow: "all-invoice",
+        inActiveClass:
+            "inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300",
+        ActiveClass:
+            "inline-block p-4 rounded-t-lg border-b-2 border-transparent text-black hover:text-gray-600 hover:border-gray-300 border-purple-500 dark:hover:text-gray-300",
+        };
+    },
+    methods: {
+        changeActive(page) {
+        this.currentShow = page;
+        let classActive = document.querySelectorAll(
+            `#tabExample>li>button#${page}`
+        );
+        if (classActive[0].id == "paid") {
+                classActive[0].classList.add("border-red-500");
+            }
+        },
+    },
+    updated() {
+        this.changeActive;
+    },
+    mounted() {},
+};
+</script>
+
+<style>
+</style> 
--- src/views/InvoicesView.vue
+++ src/views/InvoicesView.vue
@@ -1,67 +1,11 @@
 <template>
     <div class="client-page">
-        <div class="container rounded-md border-2 my-3">
+        <div class="container rounded-md border-2">
             <div class="mt-3 mb-5">
                 <h4 class="text-left text-xl fw-bolder text-black">Invoices</h4>
             </div>
+            <simple-tabs/>
             <div class="flex justify-content-between mx-2">
-                <div>
-                    <ul class="flex">
-                        <li class="mr-4">All Invoice</li>
-                        <li class="mr-4">Paid</li>
-                        <li class="mr-4">Overdue</li>
-                        <li class="mr-4">Pending</li>
-                        <li class="mr-4">Draft</li>
-                    </ul>
-                </div>
-                <div class="px-4">
-                    <button class="mx-2 bg-white text-black px-3 py-1 rounded-lg border-1 border-black">+ Add New</button>
-                    <button class="mx-2 bg-white text-black px-3 py-1 rounded-lg border-1 border-black">Import File</button>
-                </div>
-            </div>
-            <div class="rounded-xl p-3 border-2 mx-3">
-                <div class="flex justify-between">
-                    <div>
-                        <i></i>
-                        <input type="text" name="searchInvoice" placeholder="Search name, Invoice, Item" class="border-1 outline-none px-3 py-1 rounded-lg border-1 border-black">
-                    </div>
-                    <div class="flex">
-                        <div>
-                            <button class="mx-2 bg-white text-black px-3 py-1 rounded-lg border-1 border-black">Share Invoice</button>
-                        </div>
-                        <div>
-                            <button @click="toAddClient()" class="mx-2 px-3 bg-white text-black py-1 rounded-lg border-1 border-black">Delet</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-3">
-                    <table class="w-100">
-                        <tr class="text-left border-y-2">
-                            <td class="w-12 pl-3 pr-10 py-2"></td>
-                            <td class="py-3">invoice#</td>
-                            <td>Client</td>
-                            <td>Date</td>
-                            <td>Item</td>
-                            <td>Post Due</td>
-                            <td>Amount</td>
-                            <td class="text-center">Status</td>
-                        </tr>
-                        <tr v-for="(item, index) in items" :key="index" class="text-left">
-                            <td class="w-12 pl-3 pr-10 pt-3">
-                                <input type="checkbox" class="w-6 h-6">
-                            </td>
-                            <td class="pt-2">{{item.first_name}}</td>
-                            <td class="pt-2">{{item.last_name}}</td>
-                            <td class="pt-2">{{item.age}}</td>
-                            <td class="pt-2">{{item.last_name}}</td>
-                            <td class="pt-2">{{item.last_name}}</td>
-                            <td class="pt-2">{{item.last_name}}</td>
-                            <td class="w-16 px-3 pt-2">
-                                <span class="bg-gray-200 px-2">Paid</span>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
             </div>
             <div class="flex justify-end align-center mx-3 my-4">
                 <div class="ml-3 px-2 rounded border-1 border-black">1</div>
@@ -75,22 +19,10 @@
 </template>
 
 <script>
+import SimpleTabs from '@/components/SimpleTabs.vue';
 export default {
+  components: { SimpleTabs },
     name: "ClientView",
-    data() {
-        return {
-        items: [
-                { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-                { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-                { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-                { age: 38, first_name: 'Jami', last_name: 'Carney' },
-                { age: 38, first_name: 'Jami', last_name: 'Carney' },
-                { age: 38, first_name: 'Jami', last_name: 'Carney' },
-                { age: 38, first_name: 'Jami', last_name: 'Carney' },
-                { age: 38, first_name: 'Jami', last_name: 'Carney' }
-            ]
-        }
-    },
     methods: {
         toAddClient() {
             return this.$router.push('/addclient');
