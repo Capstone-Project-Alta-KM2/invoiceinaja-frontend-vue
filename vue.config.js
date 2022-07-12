1dcb232e3dddfa7c1a2ac925d9eca6ef06e9dff1
=Restu Averian Putra <=restuaverianputra123@gmail.com>
2022-07-12 20:21:28 +0700

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  pwa: {
+    name: 'invoiceinaja-frontend-vue',
+    workboxOptions: {
+      exclude: ['_redirects'],
+    }
+  }
 })
