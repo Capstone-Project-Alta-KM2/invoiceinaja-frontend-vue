9b4305a60223edf939f1660988cf7c00abbaa69c
andri-irawan79 <iandri647@gmail.com>
2022-07-13 21:30:15 +0800

    membuat tampilan halaman setting

--- src/components/blog/CommingSoonBlogPage.vue
+++ src/components/blog/CommingSoonBlogPage.vue
@@ -1,36 +1,39 @@
 <template>
-    <div class="min-w-full min-h-screen pt-28 pb-10">
-        <div class="flex flex-col items-center">
-            <div>
-                <img
-                    src="../../assets/images/landing/fall-is-coming-rafiki1.svg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <h1 class="text-3xl font-bold text-soft-purple">
-                    Comming Soon
-                </h1>
-                <p class="text-black text-lg mt-3">
-                    We are currently working building this page!
-                </p>
-            </div>
-            <div class="mt-8">
-                <button
-                    v-ripple
-                    class="button button-primary"
-                    @click="() => this.$router.push('/')"
-                >
-                    <span class="text-base px-5">Back to home</span>
-                </button>
-            </div>
-        </div>
+  <div class="min-w-full min-h-screen pt-28 pb-10">
+    <div class="flex flex-col items-center">
+      <div>
+        <img
+          src="../../assets/images/landing/fall-is-coming-rafiki1.svg"
+          alt=""
+        />
+      </div>
+      <div>
+        <h1 class="text-3xl font-bold text-soft-purple">Comming Soon</h1>
+        <p class="text-black text-lg mt-3">
+          We are currently working building this page!
+        </p>
+      </div>
+      <div v-if="currentRouter == '/blog'" class="mt-8">
+        <button
+          v-ripple
+          class="button button-primary"
+          @click="() => this.$router.push('/')"
+        >
+          <span class="text-base px-5">Back to home</span>
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ComminSoonBlogPage",
+  name: "ComminSoonBlogPage",
+  computed: {
+    currentRouter() {
+      return this.$router.currentRoute.path;
+    },
+  },
 };
 </script>
 
---
+++ src/views/SettingsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <settings-card></settings-card>
+</template>
+
+<script>
+import SettingsCard from "@/components/settings/SettingsCard.vue";
+export default {
+  components: { SettingsCard },
+  name: "SettingPage",
+};
+</script>
+
+<style></style>
